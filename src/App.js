946fc027db5cb2db2961975a45134ae376544b8d
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2023-06-14 11:29:46 +0300

    creating user data styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header';
-import UserData from './Components/UserData';
+import UserData from './Components/UserData/UserData';
 
 function App() {
   return (
--- src/Components/UserData/UserData.js
+++ src/Components/UserData/UserData.js
@@ -1,15 +1,15 @@
 import React, { useState } from 'react'
-
+import styles from './UserData.module.css'
 
 const UserData = () => {
     return (
         <form>
-            <div>
+            <div className={styles.input}>
                 <label>Enter User Name</label>
                 <input type='text' />
             </div>
 
-            <div>
+            <div className={styles.input}>
                 <label>Enter User Age</label>
                 <input type='number' />
             </div>

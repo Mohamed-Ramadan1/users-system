faf69375dbbf1d93a121bb447d53e8eab73ceb07
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2023-06-14 11:43:26 +0300

    creating the button componetn

--- src/Components/UserData/UserData.js
+++ src/Components/UserData/UserData.js
@@ -1,17 +1,34 @@
 import React, { useState } from 'react'
 import styles from './UserData.module.css'
 
+
 const UserData = () => {
+    const [userNameInput, setUserNameInput] = useState("");
+    const [ageInput, setAgeInput] = useState(null);
+
+    const changeHandler = (event,setter) => {
+        setter(event.target.value);
+    }
+    const submitHandler = (event) => {
+        event.preventDefualt()
+        resetData();
+    }
+
+    const resetData = ()=>{
+        setUserNameInput("");
+        setAgeInput(null)
+    }
+
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <div className={styles.input}>
                 <label>Enter User Name</label>
-                <input type='text' />
+                <input type='text' value={userNameInput} onChange={(event)=>changeHandler(event,setUserNameInput)} />
             </div>
 
             <div className={styles.input}>
                 <label>Enter User Age</label>
-                <input type='number' />
+                <input type='number' value={ageInput} onChange={(event) => changeHandler(event, setAgeInput)} />
             </div>
         </form>
     )
